38d87f88285c8adc95a9ef4545c82789dac673f4
Armando Mercado <armando@amercado.net>
2026-05-02 00:37:41 -0400

    Comment out stuff

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,17 +1,7 @@
--- settings for nvim stuff that needs to be configured with lua like treesitter and lsps, make sure you put nvim-lspconfig and nvim-treesitter packages in start
+-- settings for nvim stuff that needs to be configured with lua like treesitter and lsps
 local M = {}
 function M.setup()
-    local lspconfig = require('lspconfig')
-    lspconfig.vimls.setup {}
-    lspconfig.gopls.setup {}
-    lspconfig.rust_analyzer.setup {}
-    require('nvim-treesitter.configs').setup {
-        -- dont forget to install treesitter modules!
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false,
-        }
-    }
+    -- put stuff here
 end
 
 return M
